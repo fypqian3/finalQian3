60b7c0a11d510d412c0ae4adafcea23f9d6cdf96
bigsam0899 <bigsam0899@yahoo.com.hk>
2016-04-15 19:11:48 +0800

    bllballball

--- Qian3/app/src/main/java/fyp/qian3/ui/GuideAct.java
+++ Qian3/app/src/main/java/fyp/qian3/ui/GuideAct.java
@@ -1,14 +1,14 @@
 package fyp.qian3.ui;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.View;
-import android.widget.Button;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.os.Bundle;
+        import android.preference.PreferenceManager;
+        import android.view.View;
+        import android.widget.Button;
 
-import fyp.qian3.R;
+        import fyp.qian3.R;
 
 public class GuideAct extends Activity {
     Button btnSkip;
---
+++ Qian3/app/src/main/java/fyp/qian3/ui/LoadingAct.java
@@ -0,0 +1,43 @@
+package fyp.qian3.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+
+import fyp.qian3.R;
+
+public class LoadingAct extends Activity {
+    // Button btnSkip;
+    ImageView ivLoading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        ivLoading = (ImageView) findViewById(R.id.loading);
+        ivLoading.setBackgroundResource(R.drawable.animation_loading);
+
+        // Get the background, which has been compiled to an AnimationDrawable object.
+        AnimationDrawable frameAnimation = (AnimationDrawable) ivLoading.getBackground();
+
+        // Start the animation (looped playback by default).
+        frameAnimation.start();
+
+        new CountDownTimer(6000, 1200) {
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                startActivity(new Intent(LoadingAct.this, GuideAct.class));
+            }
+        }.start();
+
+    }
+}
